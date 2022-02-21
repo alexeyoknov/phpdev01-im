5a5835b9bcb4ee5b4fc55507d44cb6128f85f153
Alexey Oknov <pitrider@mail.ru>
2022-02-21 23:41:01 +0300

    Frontend: first page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,13 +13,23 @@ class DefaultController extends AbstractController
     /**
      * @throws \Exception
      */
-    public function homepage(Request $request): Response
+    public function homepage(EntityManagerInterface $em, Request $request): Response
     {
-        $number = random_int(0, 100);
-        $name = $request->query->get('name','noname');
+        $cr = $em->getRepository('App:Category');
+        $categories = $cr->findAll();
 
         return $this->render('default/index.html.twig', [
-            'name'=>$name, 'number'=>$number
+            'categories'=>$categories
+        ]);
+    }
+
+    public function category(int $id, EntityManagerInterface $em)
+    {
+        
+        $category = $em->getRepository('App:Category')->find($id);
+
+        return $this->render('default/category.html.twig', [
+            'category'=>$category
         ]);
     }
 }
